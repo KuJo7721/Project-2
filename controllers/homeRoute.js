e21e9e293a56dff84826e911c848d72369c875ef
KJ Krueger <kjkrueger12@gmail.com>
2022-09-01 18:21:17 -0700

    created profile routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -20,6 +20,25 @@ router.get('/login', (req, res) => {
 
   res.render('login');
 });
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Homework }],
+    });
+
+    const user = userData.get({ plain: true });
+    console.log (user)
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    console.log (err)
+    res.status(500).json(err);
+  }
+});
 
 
 module.exports = router;
